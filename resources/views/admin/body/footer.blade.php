efb00659b4a5514d84b3ea4c917683aab03ee010
Muhammad Khaidir <khaedir3018@gmail.com>
2025-10-08 23:05:25 +0800

    update

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col fs-13 text-muted text-center">
-                &copy; <script>document.write(new Date().getFullYear())</script> - Made with <span class="mdi mdi-heart text-danger"></span> by <a href="#!" class="text-reset fw-semibold">easylearningbd.com</a> 
+                &copy; <script>document.write(new Date().getFullYear())</script> - Bagian Pemerintahan Sekretariat Daerah Kabupaten Hulu Sungai Utara</a> 
             </div>
         </div>
     </div>
